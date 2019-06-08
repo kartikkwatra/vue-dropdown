bf72561e0b1348c960efccd7169779994d53e75d
Steve Thomas <steve@codinglabs.io>
2019-06-08 15:57:13 +1000

    styling

--- src/Dropdown.vue
+++ src/Dropdown.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="vue-dropdown">
-        <button @click="toggle" v-on-clickaway="away" class="btn-dropdown" :title="btnTitle" :class="btnClasses">
+        <button @click="toggle" v-on-clickaway="away" class="vue-dropdown-btn" :title="btnTitle" :class="btnClasses">
             <slot name="button"><i class="fas fa-caret-down"></i></slot>
         </button>
 
-        <ul v-show="show" class="menu-dropdown" :class="menuClasses">
+        <ul v-show="show" class="vue-dropdown-menu" :class="menuClasses">
             <slot name="menu"></slot>
         </ul>
     </div>
@@ -14,7 +14,7 @@
 </style>
 
 <script>
-    import { mixin as clickaway } from 'vue-clickaway'
+    import {mixin as clickaway} from 'vue-clickaway'
 
     export default {
         mixins: [clickaway],
@@ -54,6 +54,43 @@
 
 <style>
     .vue-dropdown {
-        background: red;
+        position: relative;
+    }
+
+    .vue-dropdown-btn {
+        color: #4A5568;
+    }
+
+    .vue-dropdown-btn:focus {
+        outline: none;
+    }
+
+    .vue-dropdown-menu {
+        position: absolute;
+        width: 100vw;
+        display: block;
+        border: 1px solid #EDF2F7;
+        border-top: 2px solid #EDF2F7;
+        list-style-type: none;
+        margin-top: 2rem;
+        font-size: .875rem;
+        background: #fff;
+        z-index: 50;
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+    }
+
+    .vue-dropdown-menu button {
+        width: 100%;
+        text-align: left;
+    }
+
+    .vue-dropdown-menu li:hover {
+        background: #EDF2F7;
+    }
+
+    @media only screen and (min-width: 640px) {
+        .vue-dropdown-menu {
+            width: 100%;
+        }
     }
 </style>
