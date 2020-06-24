decada2e9b9514659ca67b1036f0db67b380d470
Kartik Kwatra <kwatra.kartik@gmail.com>
2020-06-24 18:02:39 +0530

    changed svg dimentions

--- src/Dropdown.vue
+++ src/Dropdown.vue
@@ -4,7 +4,7 @@
       <slot name="button">
         <button @click="toggle" v-on-clickaway="hide" @keydown.esc="hide" type="button" :class="textClasses">
           <span v-html="text" />
-          <svg class="-mr-1 ml-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+          <svg class="-mr-1 ml-2 h-8 w-8" fill="currentColor" viewBox="0 0 20 20">
             <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"/>
           </svg>
         </button>
